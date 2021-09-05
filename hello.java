d1a46bf168ef92ec63030405b71da241c7b0cabd
nidhi527 <89890325+nidhi527@users.noreply.github.com>
2021-09-06 01:31:17 +0530

    updated to master
    updated to master

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ class Hello
 {
 public static void main(String args[])
 {
-System.out.println("hello updated done by nidz branch");
+System.out.println("hello updated done by nidz master branch");
 }
 }
 
